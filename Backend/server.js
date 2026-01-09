e126b9f66e14dd6da6ea384544fe3bf63b625a95
MihirPetkar108 <mihirpetkar2006@gmail.com>
2026-01-09 11:44:05 +0530

    Initial Server Setup done

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", chatRoutes);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to DB!");
+    } catch (err) {
+        console.log(`Failed to connect to DB ${err}`);
+    }
+};
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server listening at port ${PORT}`);
+    connectDB();
+});
